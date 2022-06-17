ce9c547fd431ef3626c73279beef415a1db4b16f
glentinashe <glentontinashemuwishi@gmail.com>
2022-06-17 07:56:55 +0200

    blogs page

---
+++ src/data/blogs.js
@@ -0,0 +1,41 @@
+import ar from '../images/svg/blogs/ar.svg'
+import call from '../images/svg/blogs/call.svg'
+import chip from '../images/svg/blogs/chip.svg'
+import cpu from '../images/svg/blogs/cpu.svg'
+
+const blogs = [
+    {
+        id:1,
+        title: 'SEO friendly design',
+        date: '01',
+        month: 'JUne',
+        image: ar,
+        link:'http://localhost:3000/blogs'
+    },
+    {
+        id:2,
+        title: 'SEO friendly design',
+        date: '01',
+        month: 'JUne',
+        image: ar,
+        link:'http://localhost:3000/blogs'
+    },
+    {
+        id:3,
+        title: 'SEO friendly design',
+        date: '01',
+        month: 'JUne',
+        image: ar,
+        link:'http://localhost:3000/blogs'
+    },
+    {
+        id:4,
+        title: 'SEO friendly design',
+        date: '01',
+        month: 'JUne',
+        image: ar,
+        link:'http://localhost:3000/blogs'
+    }
+]
+
+export default blogs;
